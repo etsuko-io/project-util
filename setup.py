b6cf7e37f82244ead3a9e4054cce6dc43b557c3c
Ruben Cronie <ruben.cronie@mediamonks.com>
2022-06-16 11:35:16 +0200

    git structure

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="project-util",
     version="0.0.1",
-    packages=find_packages(include=["src", "src.*"]),
+    packages=find_packages(
+        where='src',
+    ),
+    package_dir={"": "src"},
     install_requires=[],
 )
